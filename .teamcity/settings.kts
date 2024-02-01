29a4114158d1cadf4966e9f046453f86ec3d7022
Arran White <arran.white@hey.com>
2024-02-01 11:18:27 +0000

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,49 +1,30 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
-version = "2022.10"
+version = "2023.05"
 
 project {
-
     buildType(Build)
 }
 
-object Build : BuildType({
-    name = "Build"
+fun createBuildType(name: String, scriptContent: String): BuildType {
+    return BuildType({
+        this.name = name
 
-    params {
-        param("env.BUILD_ID", "%teamcity.build.id%")
-    }
+        params {
+            param("env.BUILD_ID", "%teamcity.build.id%")
+        }
 
-    steps {
-        
-        powerShell {
-            name = "Test"
-            scriptMode = script {
-                content = """Write-Host "OK""""
+        steps {
+            powerShell {
+                name = "Test"
+                scriptMode = script {
+                    content = scriptContent
+                }
             }
         }
-    }
-})
+    })
+}
+
+object Build : BuildType by createBuildType("Build", """Write-Host "OK"""")
+object AnotherBuild : BuildType by createBuildType("AnotherBuild", """Write-Host "Another OK"""")
